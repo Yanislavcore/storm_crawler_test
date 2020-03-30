7ba866e8274bf459cc4a7f8afdb1f76d1295bc19
Yanislav Kornev <yanislavcore@gmail.com>
2020-03-30 15:06:16 +0300

    More tests

---
+++ src/test/java/org/yanislavcore/components/EsIndexerTest.java
@@ -0,0 +1,82 @@
+package org.yanislavcore.components;
+
+import com.codahale.metrics.Counter;
+import com.digitalpebble.stormcrawler.Metadata;
+import com.digitalpebble.stormcrawler.persistence.Status;
+import org.apache.storm.Testing;
+import org.apache.storm.task.TopologyContext;
+import org.apache.storm.testing.MkTupleParam;
+import org.apache.storm.tuple.Tuple;
+import org.apache.storm.tuple.Values;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.yanislavcore.es.PagesIndexDao;
+import org.yanislavcore.utils.TimeMachine;
+
+import java.time.LocalDate;
+import java.time.ZoneOffset;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TimeZone;
+
+import static org.mockito.Mockito.*;
+
+class EsIndexerTest {
+
+    private final long start = System.currentTimeMillis();
+    private final LocalDate startDate = LocalDate.now(ZoneOffset.UTC);
+    private PagesIndexDao daoMock;
+    private TimeMachine mockTimeMachine;
+    private EsIndexer indexer;
+
+    @BeforeEach
+    void setUp() {
+        daoMock = mock(PagesIndexDao.class);
+        mockTimeMachine = mock(TimeMachine.class);
+        when(mockTimeMachine.epochMillis()).thenReturn(start);
+        when(mockTimeMachine.todayUtc()).thenReturn(startDate);
+        indexer = new EsIndexer((conf) -> daoMock, mockTimeMachine);
+        Map<String, Object> conf = Map.of(
+                "pagesIndexer.indexBufferSize", 10L,
+                "pagesIndexer.indexBufferTimeoutMillis", 10L,
+                "pagesIndex", "pages"
+        );
+        TopologyContext mockTopologyContext = mock(TopologyContext.class);
+        doReturn(mock(Counter.class)).when(mockTopologyContext).registerCounter(any());
+        doNothing().when(daoMock).indexPages(anyString(), anyList(), any());
+        indexer.prepare(conf, mockTopologyContext, null);
+    }
+
+    @Test
+    void shouldSendBatchAfterBufferIsFull() {
+        MkTupleParam param = new MkTupleParam();
+        param.setFields("url", "status", "metadata");
+        Tuple t = Testing.testTuple(
+                new Values("http://test.com/path?query=id", Status.DISCOVERED, new Metadata(new HashMap<>())),
+                param
+        );
+        for (int i = 0; i < 100; i++) {
+            indexer.execute(t);
+        }
+        verify(daoMock, times(10)).indexPages(anyString(), anyList(), any());
+        verifyNoMoreInteractions(daoMock);
+    }
+
+    @Test
+    void shouldSendBatchAfterTimeout() {
+        MkTupleParam param = new MkTupleParam();
+        param.setFields("url", "status", "metadata");
+        Tuple t = Testing.testTuple(
+                new Values("http://test.com/path?query=id", Status.DISCOVERED, new Metadata(new HashMap<>())),
+                param
+        );
+        for (int i = 0; i < 5; i++) {
+            indexer.execute(t);
+        }
+        verifyNoInteractions(daoMock);
+        when(mockTimeMachine.epochMillis()).thenReturn(start + 10);
+        indexer.execute(t);
+        verify(daoMock, times(1)).indexPages(anyString(), anyList(), any());
+        verifyNoMoreInteractions(daoMock);
+    }
+}
