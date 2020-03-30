241a7cff8cb282801e0ac3642f663441b3125ff6
Yanislav Kornev <yanislavcore@gmail.com>
2020-03-30 14:27:22 +0300

    Serializable fix

--- src/main/java/org/yanislavcore/utils/TimeMachine.java
+++ src/main/java/org/yanislavcore/utils/TimeMachine.java
@@ -1,12 +1,13 @@
 package org.yanislavcore.utils;
 
 import javax.annotation.Nonnull;
+import java.io.Serializable;
 import java.time.LocalDate;
 
 /**
  * Separates time operations in order to simplify time-based tests.
  */
-public interface TimeMachine {
+public interface TimeMachine extends Serializable {
     /**
      * Returns current date in UTC timezone.
      * @return current date
